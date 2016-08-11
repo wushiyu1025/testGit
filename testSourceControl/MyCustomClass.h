11a13aef3318c56ec2dcf1304ac4187338e72820
wushiyu1025 <macbookair@ki-houmatoMacBook-Air.local>
2016-08-11 16:55:03 +0800

    addFile
    添加了文件

---
+++ testSourceControl/MyCustomClass.h
@@ -0,0 +1,13 @@
+//
+//  MyCustomClass.h
+//  testSourceControl
+//
+//  Created by shizb macbook air on 16/8/11.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCustomClass : NSObject
+
+@end
---
+++ testSourceControl/MyCustomClass.m
@@ -0,0 +1,13 @@
+//
+//  MyCustomClass.m
+//  testSourceControl
+//
+//  Created by shizb macbook air on 16/8/11.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import "MyCustomClass.h"
+
+@implementation MyCustomClass
+
+@end
