742bff3feece267b4cad5276d5dbcbdfcd52609b
wushiyu1025 <macbookair@ki-houmatoMacBook-Air.local>
2016-08-11 16:47:25 +0800

    testGit
    demo

---
+++ testSourceControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSourceControl
+//
+//  Created by shizb macbook air on 16/8/10.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testSourceControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testSourceControl
+//
+//  Created by shizb macbook air on 16/8/10.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
